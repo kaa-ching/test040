7f025e304045b1fac3a018047a31125af736d588
Klaas van Gend <klaas@opentux.nl>
2018-03-01 16:13:04 +0100

    Spark

---
+++ SparkPlug.cpp
@@ -0,0 +1,14 @@
+#include "SparkPlug.h"
+
+SparkPlug::SparkPlug(int cylNr)
+{
+}
+
+SparkPlug::~SparkPlug()
+{
+}
+
+void SparkPlug::spark()
+{
+    std::cout << "Spark" << std::endl;
+}
---
+++ SparkPlug.h
@@ -0,0 +1,13 @@
+#ifndef SPARKPLUG_H
+#define SPARKPLUG_H
+
+class SparkPlug
+{
+public:
+    explicit SparkPlug();
+    virtual ~SparkPlug();
+
+    void spark();
+};
+
+#endif // ENGINE_H
