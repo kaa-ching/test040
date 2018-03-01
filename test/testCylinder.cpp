a6743c18d1b6066df190358dc150013496e2f0c9
Klaas van Gend <klaas@opentux.nl>
2018-03-01 14:20:08 +0100

    Nice vs Strict mock

--- test/testCylinder.cpp
+++ test/testCylinder.cpp
@@ -3,6 +3,8 @@
 #include "gmock/gmock.h"
 
 using ::testing::_;
+using ::testing::NiceMock;
+using ::testing::StrictMock;
 using ::testing::Return;
 using ::testing::ReturnRef;
 using ::testing::Invoke;
@@ -21,6 +23,7 @@ public:
     MOCK_METHOD1(CloseExhaust, void(int nr));
 };
 
+template <class IntakeType, class ExhaustType>
 class CylinderTest : public ::testing::Test
 {
 public:
@@ -29,13 +32,15 @@ public:
     {
     }
     
-    MockIntake m_intake;
-    MockExhaust m_exhaust;
+    IntakeType m_intake;
+    ExhaustType m_exhaust;
     Cylinder m_cylinder;
 };
 
+using CylinderTestNice = CylinderTest<NiceMock<MockIntake>, NiceMock<MockExhaust>>;
+using CylinderTestStrict = CylinderTest<StrictMock<MockIntake>, StrictMock<MockExhaust>>;
 
-TEST_F(CylinderTest, Test_Cylinder_StartPhase)
+TEST_F(CylinderTestNice, Test_Cylinder_StartPhase)
 {
     EXPECT_EQ(m_cylinder.getStroke(), Cylinder::Stroke::INTAKE);
 
@@ -52,7 +57,7 @@ TEST_F(CylinderTest, Test_Cylinder_StartPhase)
     EXPECT_EQ(myCylinder7.getStroke(), Cylinder::Stroke::EXHAUST);
 }
 
-TEST_F(CylinderTest, Test_Cylinder_Rotate)
+TEST_F(CylinderTestNice, Test_Cylinder_Rotate)
 {
     EXPECT_EQ(m_cylinder.getStroke(), Cylinder::Stroke::INTAKE);
     m_cylinder.rotate();
@@ -65,8 +70,17 @@ TEST_F(CylinderTest, Test_Cylinder_Rotate)
     EXPECT_EQ(m_cylinder.getStroke(), Cylinder::Stroke::INTAKE);
 }
 
-TEST_F(CylinderTest, Test_Cylinder_ChangeIntake)
+TEST_F(CylinderTestStrict, Test_Cylinder_ChangeIntake)
 {
     EXPECT_CALL(m_intake, CloseIntake(0));
     m_cylinder.rotate();
+}
+
+TEST_F(CylinderTestStrict, Test_Cylinder_ChangeExhaust)
+{
+    Cylinder myCylinder3(3, m_intake, m_exhaust);
+
+    EXPECT_CALL(m_intake, OpenIntake(_));
+    EXPECT_CALL(m_exhaust, CloseExhaust(3));
+    myCylinder3.rotate();
 }
